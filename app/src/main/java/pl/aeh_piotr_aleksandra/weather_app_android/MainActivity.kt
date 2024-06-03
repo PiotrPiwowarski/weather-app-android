530c4bac4c8be0535e6e4f5287ff6578086ebc30
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-06-03 21:22:41 +0200

    init commit

---
+++ app/src/main/java/pl/aeh_piotr_aleksandra/weather_app_android/MainActivity.kt
@@ -0,0 +1,46 @@
+package pl.aeh_piotr_aleksandra.weather_app_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pl.aeh_piotr_aleksandra.weather_app_android.ui.theme.WeatherappandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherappandroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherappandroidTheme {
+        Greeting("Android")
+    }
+}
